24f4c582888ffd74135f224c9a11805743555ec9
Aathish101 <aathishvs2@gmail.com>
2026-02-20 06:55:23 +0530

    Fix LoginPage api path

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useAuth } from '../contexts/AuthContext';
 import { useNavigate } from 'react-router-dom';
-import api from '../api'
+import api from '../utils/api'
 import LoadingSpinner from '../components/LoadingSpinner';
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from "../firebase";
